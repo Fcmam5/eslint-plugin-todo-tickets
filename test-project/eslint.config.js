d55d7003b5504a331cbda27522fa0796da3af4a9
Abdeldjalil Fortas <9090674+Fcmam5@users.noreply.github.com>
2025-10-07 21:20:03 +0200

    ci: add Github env

--- test-project/eslint.config.js
+++ test-project/eslint.config.js
@@ -1,23 +1,26 @@
-import todoTickets from 'eslint-plugin-todo-tickets';
+import todoTickets from "eslint-plugin-todo-tickets";
 
 export default [
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: 'module',
+      sourceType: "module",
     },
     plugins: {
-      'todo-tickets': todoTickets
+      "todo-tickets": todoTickets,
     },
     rules: {
-      'todo-tickets/todo-tickets': ['error', {
-        ticketPatterns: [
-          '[A-Z]{2,}-\\d+',  // JIRA format (e.g., ABC-123)
-          '#\\d+'            // GitHub format (e.g., #42)
-        ],
-        keywords: ['TODO', 'FIXME', 'BUG', 'HACK']
-      }]
-    }
-  }
+      "todo-tickets/todo-tickets": [
+        "error",
+        {
+          ticketPatterns: [
+            "[A-Z]{2,}-\\d+", // JIRA format (e.g., ABC-123)
+            "#\\d+", // GitHub format (e.g., #42)
+          ],
+          keywords: ["TODO", "FIXME", "BUG", "HACK"],
+        },
+      ],
+    },
+  },
 ];
